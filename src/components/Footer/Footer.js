54a97f3d594d0c2e9831aabd2768d61013733696
And Aydin <55938221+andaydin@users.noreply.github.com>
2021-07-24 15:42:51 +1000

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,10 +4,6 @@ function Footer() {
     return (
         <footer class="page-footer font-small blue footer">
 
-            <div class="footer-copyright text-center py-3">Made with ❤ by
-        <a href="https://about.me/adewaletoluwani" target="_blank" rel="noopener noreferrer" className="author"> Toluwani Adewale</a>
-            </div>
-
         </footer>
     );
 }
