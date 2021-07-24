1727ee97b7e67c813cda51269764660a561d0f17
And Aydin <55938221+andaydin@users.noreply.github.com>
2021-07-24 15:44:43 +1000

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,10 @@ function Footer() {
     return (
         <footer class="page-footer font-small blue footer">
 
+            <div class="footer-copyright text-center py-3">Made with ❤ by
+                    <a href="https://about.me/adewaletoluwani" target="_blank" rel="noopener noreferrer" className="author"> Andy Arest</a>
+            </div>
+
         </footer>
     );
 }
